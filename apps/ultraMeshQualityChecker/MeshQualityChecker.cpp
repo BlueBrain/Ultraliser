d04621e2a75b705de94df38ed20843be45ebce8b
Marwan Abdellah <abdellah.marwan@gmail.com>
2021-05-12 21:54:17 +0200

    Adding the QualityChecker.

---
+++ apps/ultraMeshQualityChecker/MeshQualityChecker.cpp
@@ -0,0 +1,110 @@
+/*******************************************************************************
+ * Copyright (c) 2016 - 2019
+ * Blue Brain Project (BBP) / Ecole Polytechniqe Federale de Lausanne (EPFL)
+ * Author(s): Marwan Abdellah <marwan.abdellah@epfl.ch>
+ *
+ * This file is part of Ultraliser <https://github.com/BlueBrain/Ultraliser>
+ *
+ * This library is free software; you can redistribute it and/or modify it under
+ * the terms of the GNU Lesser General Public License version 3.0 as published
+ * by the Free Software Foundation.
+ *
+ * This library is distributed in the hope that it will be useful, but
+ * WITHOUT ANY WARRANTY; without even the implied warranty of MERCHANTABILITY
+ * or FITNESS FOR A PARTICULAR PURPOSE.
+ *
+ * See the GNU Lesser General Public License for more details.
+ *
+ * You should have received a copy of the GNU Lesser General Public License
+ * along with this library; if not, write to the Free Software Foundation,
+ * Inc., 51 Franklin Street, Fifth Floor, Boston, MA 02110-1301 USA.
+ ******************************************************************************/
+
+#include <Ultraliser.h>
+#include "Args.h"
+
+Options* parseArguments(Args* args)
+{
+    Ultraliser::Argument inputMesh(
+                "--mesh",
+                ARGUMENT_TYPE::STRING,
+                "The full path to the mesh.",
+                ARGUMENT_PRESENCE::MANDATORY);
+    args->addArgument(&inputMesh);
+
+    Ultraliser::Argument outputDirectory(
+                "--output-directory",
+                ARGUMENT_TYPE::STRING,
+                "Output directory where the results will be generated.",
+                ARGUMENT_PRESENCE::MANDATORY);
+    args->addArgument(&outputDirectory);
+
+    Ultraliser::Argument prefix(
+                "--prefix",
+                ARGUMENT_TYPE::STRING,
+                "Just a prefix that will be used to label the output files. "
+                "If this is not given by the user, the name of the mesh file "
+                "will be used.");
+    args->addArgument(&prefix);
+
+    // Parse the command line options
+    args->parse();
+
+    // Construct the options
+    Options* options = new Options();
+
+    // Get all the options
+    options->inputMesh = args->getStringValue(&inputMesh);
+    options->outputDirectory = args->getStringValue(&outputDirectory);
+    options->prefix = args->getStringValue(&prefix);
+
+    /// Validate the arguments
+    if (!Ultraliser::File::exists(options->inputMesh))
+    {
+        LOG_ERROR("The file [%s] does NOT exist! ", options->inputMesh.c_str());
+    }
+
+    // Try to make the output directory
+    mkdir(options->outputDirectory.c_str(), 0777);
+    if (!Ultraliser::Directory::exists(options->outputDirectory))
+    {
+        LOG_ERROR("The directory [%s] does NOT exist!",
+                  options->outputDirectory.c_str());
+    }
+
+    // If no prefix is given, use the file name
+    if (options->prefix == NO_DEFAULT_VALUE)
+    {
+        options->prefix = Ultraliser::File::getName(options->inputMesh);
+    }
+
+    // Construct the output prefix
+    options->outputPrefix = options->outputDirectory + "/" + options->prefix;
+
+
+    LOG_TITLE("Ultralizing");
+
+    // Return the executable options
+    return options;
+}
+
+int main(int argc , const char** argv)
+{
+    // Arguments
+    Args args(argc, argv,
+              "This tool checks thq quality of the input mesh and report a "
+              "file with all the measures.");
+
+    // Parse the arguments and get the values
+    Options* options = parseArguments(&args);
+
+    /// Load the mesh
+    Ultraliser::Mesh* inputMesh = new Ultraliser::Mesh(options->inputMesh);
+
+    inputMesh->printStats("input", &options->outputPrefix);
+
+    // Free
+    inputMesh->~Mesh();
+
+    ULTRALISER_DONE;
+}
